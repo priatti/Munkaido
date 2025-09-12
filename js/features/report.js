799fbe3e73d007b76c5ff527f732f0958742cdc0
priatti <priatti11@gmail.com>
2025-09-12 09:21:37 +0200

    Add files via upload

--- js/features/report.js
+++ js/features/report.js
@@ -360,6 +360,6 @@ function renderMonthlyReportPreview() {
 // Tiny util
 function escapeHtml(s) {
     return String(s || '').replace(/[&<>"']/g, m => ({
-        '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;','\\'':'&#39;'
+        '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"\\'":"&#39;"
     })[m]);
 }
