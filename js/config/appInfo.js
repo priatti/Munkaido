735f7ea2e282c15ff918b06f086998cd44dce24d
priatti <priatti11@gmail.com>
2025-09-17 23:45:47 +0200

    Update appInfo.js

--- js/config/appInfo.js
+++ js/config/appInfo.js
@@ -4,7 +4,7 @@
     year: "2025",
     authorHu: "Princz Attila",
     authorDe: "Attila Princz",
-    copyrightHu: function(){ return "© " + this.year + " " + this.authorHu + ". Szabadon terjeszthető."; },
-    copyrightDe: function(){ return "© " + this.year + " " + this.authorDe + ". Frei weiterverbreitbar."; }
+    copyrightHu: function(){ return "© " + this.year + " " + this.authorHu + ". Minden jog fenntartva."; },
+    copyrightDe: function(){ return "© " + this.year + " " + this.authorDe + ". Alle Rechte vorbehalten."; }
   };
 })();
