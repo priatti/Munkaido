1a80ccba001ab54aa98321fae44026a44e4f61f5
priatti <priatti11@gmail.com>
2025-10-11 14:57:16 +0200

    Add files via upload

---
+++ v4.01.01/js/config/appInfo.js
@@ -0,0 +1,14 @@
+(function(){
+  window.APP_INFO = {
+    version: '3.03.03', // ← FRISSÍTVE
+    year: "2025",
+    authorHu: "Princz Attila",
+    authorDe: "Attila Princz",
+    copyrightHu: function(){ return "© " + this.year + " " + this.authorHu + ". Minden jog fenntartva."; },
+    copyrightDe: function(){ return "© " + this.year + " " + this.authorDe + ". Alle Rechte vorbehalten."; }
+  };
+})();
+
+// Copyright & Contact
+const appCopyright = '© 2025 GuriGO';
+const appContact = 'info@gurigo.eu';
